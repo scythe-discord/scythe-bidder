cc59946c6b3636422fa5c951dee4121e2bfa0029
Rezende <vgrezende@gmail.com>
2020-04-25 01:02:21 -0300

    Update server message

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ import { Server } from 'boardgame.io/server';
 const PORT = process.env.PORT || 8000;
 const server = Server({ games: [ScytheBidderGame] });
 server.run(PORT, () => {
-  console.log(`Serving at: http://localhost:${PORT}/`);
+  console.log(`Serving at: http://gaming-lobby-server/herokuapp.com:${PORT}/`);
 });
