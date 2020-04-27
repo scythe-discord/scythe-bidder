4635dd349cba7414ada4acae9fbfe7dac0519926
Rezende <vgrezende@gmail.com>
2020-04-27 12:20:43 -0300

    Server stuff

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+const os = require('os');
 import { default as ScytheBidderGame } from './scythe-bidder/game';
 import { Server } from 'boardgame.io/server';
 
+const hostname = os.hostname();
 const PORT = process.env.PORT || 8000;
 const server = Server({ games: [ScytheBidderGame] });
 server.run(PORT, () => {
-  console.log(`Serving at: http://gaming-lobby-server/herokuapp.com:${PORT}/`);
+  console.log(`Serving at: ${hostname}:${PORT}/`);
 });
