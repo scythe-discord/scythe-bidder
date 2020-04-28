97687e58ebb080fb1e5ff856e1df6ba05ecdb024
Rezende <vgrezende@gmail.com>
2020-04-28 18:57:57 -0300

    Change to local multiplayer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import LobbyView from './lobby/lobby'
+import { Client } from 'boardgame.io/react';
+import { Local } from 'boardgame.io/multiplayer';
+import ScytheBidderGame from './scythe-bidder/game';
+import BiddingBoard from './scythe-bidder/board';
+
+const ScytheClient = Client({
+  game: ScytheBidderGame,
+  board: BiddingBoard,
+  multiplayer: Local(),
+  numPlayers: 4,
+});
 
 const App = () => (
-  <LobbyView />
+  <div>
+    <ScytheClient playerID="0"/>
+    <ScytheClient playerID="1"/>
+    <ScytheClient playerID="2"/>
+    <ScytheClient playerID="3"/>
+  </div>
 );
 
 export default App
