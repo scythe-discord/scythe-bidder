f73e04f6e2007e971453dab6c75e152994edfd45
Rezende <vgrezende@gmail.com>
2020-04-29 18:21:14 -0300

    Add key to gameLogger

--- src/scythe-bidder/gamelog.js
+++ src/scythe-bidder/gamelog.js
@@ -2,14 +2,11 @@ import React from "react";
 
 const GameLog = props => {
   const { G } = props;
-  console.log(G.gameLogger);
   return (
     <ul className={"list-group"}>
-      {G.gameLogger.map(msg => {
-        return (
-          <li className={"list-group-item"}>{msg}</li>
-        )
-      })}
+      {G.gameLogger.map((msg, key) => (
+        <li className={"list-group-item"} key={key}>{msg}</li>
+      ))}
     </ul>
   )
 };
