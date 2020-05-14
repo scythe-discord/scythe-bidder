3a51e3909837db36b0abdf57e5ea01b838cad354
Rezende <smugskull@pm.me>
2020-05-14 12:51:45 -0300

    Update lobby and server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
-const os = require('os');
-import { default as ScytheBidderGame } from './scythe-bidder/game';
 import { Server } from 'boardgame.io/server';
+import path from 'path';
+import serve from 'koa-static';
+import { default as ScytheBidderGame } from './scythe-bidder/game';
 
-const hostname = os.hostname();
-const PORT = process.env.PORT || 8000;
 const server = Server({ games: [ScytheBidderGame] });
+const PORT = process.env.PORT || 8000;
+
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath));
+
 server.run(PORT, () => {
-  console.log(`Serving at: ${hostname}:${PORT}/`);
+  server.app.use(
+    async (ctx, next) => await serve(frontEndAppBuildPath)(
+      Object.assign(ctx, {path: 'index.html' }),
+      next
+    )
+  )
 });
