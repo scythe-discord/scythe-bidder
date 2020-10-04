34570f1947242cf8ebf89eb5eca3bafd6dda1916
Germy <27422540+lightyearsaway@users.noreply.github.com>
2020-10-04 14:00:07 -0700

    Re-add routes

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import App from "./App";
+
+const routes = [
+  {
+    name: "Lobby",
+    routes: [
+      {
+        path: "/",
+        text: "Lobby",
+        component: App,
+      },
+    ],
+  },
+];
+
+export default routes;
