690a02add0a36850be650af2593a5738ce7958d4
Germy <27422540+lightyearsaway@users.noreply.github.com>
2020-10-12 20:48:51 -0700

    Add koa-sslify to force redirect to https

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,17 @@ import { Server } from "boardgame.io/server";
 import path from "path";
 import serve from "koa-static";
 import { default as ScytheBidderGame } from "../src/scythe-bidder/game";
+import {
+  default as sslify,
+  xForwardedProtoResolver as resolver,
+} from "koa-sslify";
 
 const server = Server({ games: [ScytheBidderGame] });
 const PORT = Number(process.env.PORT) || 8000;
+
+if (process.env.NODE_ENV === "production") {
+  server.app.use(sslify({ resolver }));
+}
 
 const frontEndAppBuildPath = path.resolve(__dirname, "../build");
 server.app.use(serve(frontEndAppBuildPath));
