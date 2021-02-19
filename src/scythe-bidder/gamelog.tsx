7e8a9344c0d33a51b399f1585fd205d78cd8ca04
Nick <nrsimonelli@gmail.com>
2021-02-19 16:35:32 -0600

    fixed list coloring in bid room

--- src/scythe-bidder/gamelog.tsx
+++ src/scythe-bidder/gamelog.tsx
@@ -11,13 +11,16 @@ const GameLog = (props: { G: GameState }) => {
       dataSource={G.gameLogger}
       renderItem={(msg, key) => (
         <List.Item
-          css={{ background: "white", padding: "12px 24px" }}
+          css={{
+            background: "var(--background-list-body)",
+            padding: "12px 24px",
+          }}
           key={key}
         >
           {msg}
         </List.Item>
       )}
-      css={{ background: "#fff", marginTop: 24 }}
+      css={{ marginTop: 24 }}
     ></List>
   );
 };
