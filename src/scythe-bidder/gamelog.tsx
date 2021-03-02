3e70c4db227816d06deb2b2a5175cc7eb33ba172
nrsimonelli <nrsimonelli@gmail.com>
2021-03-01 23:33:24 -0600

    replaced old var with theme object in gamelog

--- src/scythe-bidder/gamelog.tsx
+++ src/scythe-bidder/gamelog.tsx
@@ -1,18 +1,20 @@
 /** @jsx jsx */
 
 import { jsx } from "@emotion/core";
+import { useTheme } from "@emotion/react";
 import { List } from "antd";
 import { GameState } from "./types";
 
 const GameLog = (props: { G: GameState }) => {
   const { G } = props;
+  const theme = useTheme();
   return (
     <List
       dataSource={G.gameLogger}
       renderItem={(msg, key) => (
         <List.Item
           css={{
-            background: "var(--background-list-body)",
+            background: theme.listBody,
             padding: "12px 24px",
           }}
           key={key}
